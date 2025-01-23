da0a303d8f8bc1db2dd3f0cb2fb46b6144ee480a
developer <developer@innovativemojo.com>
2025-01-23 12:30:06 +0500

    cloudinary and multer configuration is done

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,14 @@
+import multer from "multer";
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    //understanding the parameters of the function which are req, file and cb
+    cb(null, "./public/temp");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + "-" + uniqueSuffix);
+    console.log("File log of multer", file);
+  },
+});
+
+export const upload = multer({ storage: storage });
